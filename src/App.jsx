9bff8d54ab1613ea3ecebc3d005a074a293e4269
Concurr3ncia <kevinsd872@gmail.com>
2025-03-24 16:04:20 -0500

    Add keep-alive ping to web service in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import AppRouter from "./routes/AppRouter";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <AppRouter />
-  );
+  useEffect(() => {
+    const keepAlive = setInterval(() => {
+      fetch("https://tu-web-service.onrender.com/ping")
+        .then(() => console.log("Ping enviado"))
+        .catch((e) => console.error("Ping fallido", e));
+    }, 5 * 60 * 1000);
+
+    return () => clearInterval(keepAlive);
+  }, []);
+
+  return <AppRouter />;
 }
 
 export default App;
