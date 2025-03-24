c42b0566cab1312445381f57ab01cf56a171bb85
Concurr3ncia <kevinsd872@gmail.com>
2025-03-24 18:14:24 -0500

    Refactor navbar component to use new icons and improve styling for better user experience

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Header from "../components/header";
 import "../styles/global.css";
-import { IoMenu } from "react-icons/io5";
+import { BiMenu, BiMenuAltLeft } from "react-icons/bi";
+import { IoIosLogIn } from "react-icons/io";
+import { FaHome } from "react-icons/fa";
 
 function Login() {
   const [navbarVisible, setNavbarVisible] = useState(false);
@@ -14,13 +16,24 @@ function Login() {
   return (
     <div className="flex md:hidden">
       <div className="flex flex-col gap-20 text-white relative m-5 z-15">
-        <IoMenu
-          className="text-4xl text-white"
-          onClick={toggleNavbarVisibility}
-        />
+        {navbarVisible ? (
+          <BiMenuAltLeft
+            className="text-4xl text-white cursor-pointer"
+            onClick={toggleNavbarVisibility}
+          />
+        ) : (
+          <BiMenu
+            className="text-4xl text-white cursor-pointer"
+            onClick={toggleNavbarVisibility}
+          />
+        )}
       </div>
 
-      <div className={`absolute flex flex-col justify-start items-start pt-15 gap-3 pl-5 text-white bg-[#476281] h-screen w-30 z-10 transition-all duration-200 ${navbarVisible ? "left-0" : "-left-full"}`}>
+      <div
+        className={`absolute flex flex-col justify-start items-center pt-20 gap-3 text-white bg-[#569bfe] h-screen w-40 z-10 transition-all duration-200 ${
+          navbarVisible ? "left-0" : "-left-full"
+        }`}
+      >
         <div className="flex items-start justify-center md:mr-20">
           <h1 className="text-2xl text-star-white font-medium text-center">
             Noctis
@@ -29,27 +42,21 @@ function Login() {
 
         <a
           href="/"
-          className="md:mr-5 text-star-white font-medium cursor-pointer rounded transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2 md:mr-5 text-star-white font-medium bg-blue-500 w-[85%] p-2 cursor-pointer rounded-xl transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
+        > <FaHome className="text-2xl text-white" />
           Home
-        </a>
-        <a
-          href="/about"
-          className="md:mr-auto text-star-white font-medium cursor-pointer rounded transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
-          About
         </a>
 
         <a
           href="/register"
-          className="text-star-white font-medium rounded-xl w-25 cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2 text-star-white font-medium bg-blue-500 w-[85%] p-2 rounded-xl w-25 cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
+        > <IoIosLogIn className="text-2xl text-white" />
           Sign up
         </a>
         <a
           href="/login"
-          className="text-star-white font-medium bg-blue-600 w-fit p-2 rounded-xl w-25 text-center cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-500 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2 text-star-white font-medium bg-blue-600 w-[85%] p-2 rounded-xl w-25 text-start cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-500 hover:shadow-lg"
+        > <IoIosLogIn className="text-2xl text-white" />
           Sign in
         </a>
       </div>
