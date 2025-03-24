0ff6f6503591f7b991ac3a63050cdb239259f183
Concurr3ncia <kevinsd872@gmail.com>
2025-03-24 03:56:10 -0500

    Set preview host for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: 'noctis-l37u.onrender.com',
   plugins: [react(), tailwindcss()],
 })
