a795303f75b104791cfb51bf73d9f2c9c92a6b52
Concurr3ncia <kevinsd872@gmail.com>
2025-03-24 18:14:19 -0500

    Enhance Header component with icons and improve layout for better accessibility

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
 import "../styles/global.css";
+import { IoIosLogIn } from "react-icons/io";
+import { FaHome } from "react-icons/fa";
 
 function Header() {
   return (
     <>
-      <header className="hidden w-screen md:flex flex-col items-center text-center justify-start pl-15 pt-10 pr-15 text-white md:flex-row gap-5 md:items-center">
+      <header className="hidden w-screen md:flex flex-col items-center text-center justify-start pl-10 pt-5 pr-10 text-white md:flex-row gap-5 md:items-center">
         <div className="flex items-center justify-center md:mr-20">
           <h1 className="text-2xl text-star-white font-medium text-center">
             Noctis
@@ -12,29 +14,26 @@ function Header() {
 
         <a
           href="/"
-          className="md:mr-5 text-star-white font-medium cursor-pointer p-2 rounded transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2 md:mr-5 text-star-white font-medium cursor-pointer p-2 rounded transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
+        > <FaHome className="text-2xl text-white" />
           Home
         </a>
-        <a
-          href="/about"
-          className="md:mr-auto text-star-white font-medium cursor-pointer p-2 rounded transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
-          About
-        </a>
 
+        <div className="flex flex-row gap-5 ml-auto">
         <a
           href="/register"
-          className="text-star-white font-medium bg-[] p-2 rounded-xl w-25 text-center cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2 text-star-white font-medium p-2 rounded-xl w-fit text-center cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-400 hover:shadow-lg"
+        > <IoIosLogIn className="text-2xl text-white" />
           Sign up
         </a>
         <a
           href="/login"
-          className="text-star-white font-medium bg-blue-600 p-2 rounded-xl w-25 text-center cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-500 hover:shadow-lg"
-        >
+          className="flex flex-row gap-2  text-star-white font-medium bg-blue-600 p-2 rounded-xl w-fit text-center cursor-pointer transition-all duration-200 transform hover:scale-110 hover:bg-blue-500 hover:shadow-lg"
+        > <IoIosLogIn className="text-2xl text-white" />
           Sign in
         </a>
+        </div>
+        
       </header>
     </>
   );
