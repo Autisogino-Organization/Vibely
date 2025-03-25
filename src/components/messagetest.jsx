7623d6a021d635a1d75cbcc9b36854de1e9c145a
Concurr3ncia <kevinsd872@gmail.com>
2025-03-25 02:11:17 -0500

    Add MessageTest component for testing message display functionality

---
+++ src/components/messagetest.jsx
@@ -0,0 +1,162 @@
+import Message from "../components/message";
+
+const gacc =
+  "https://images-ext-1.discordapp.net/external/ZZVZlX8hJvqmvT-FxUWQzxjK0HRwJeDdocyY1nFGfUI/%3Fsize%3D512/https/cdn.discordapp.com/avatars/838793520554442752/eda3b21d9bef5e6f78f6198bac4ac9cf.png?format=webp&quality=lossless&width=172&height=172";
+const safe =
+  "https://images-ext-1.discordapp.net/external/8E859V6oMI1MYX_9Hi4uDt1vAhrU_xfXb4QHI3Qxofs/%3Fsize%3D512/https/cdn.discordapp.com/avatars/778788944024043540/6c1457c13793d3fe3d2cc650e27b04ba.png?format=webp&quality=lossless&width=172&height=172";
+const keven =
+  "https://images-ext-1.discordapp.net/external/hcm5ZAl_ouNLQ2inH8JvHv0WB7PFCC1YDxO4BsuuOmQ/%3Fsize%3D512/https/cdn.discordapp.com/avatars/1287258677706883183/74fb24296c29ea39a63899122c5e1811.png?format=webp&quality=lossless&width=343&height=343";
+
+function MessageTest() {
+  return (
+    <>
+      <Message
+        pfp={gacc}
+        message="Sep, entonces yo q podría hacer xD"
+        author="gacc_15"
+      />
+      <Message pfp={safe} message="elige register o login" author="Safe" />
+      <Message
+        pfp={gacc}
+        message="Iré por lo difícil Register"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="vale pues haz esa parte y yo hago el login"
+        author="Safe"
+      />
+      <Message
+        pfp={keven}
+        message="Se viene lo chido"
+        author="! IrregularDev"
+      />
+      <Message
+        pfp={gacc}
+        message="Una pregunta, yo haría la db?"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="Pues depende, yo tambien quiero aprender a hacerla, entonces supongo que podriamos dividirnos eso si, habria que tener muy buena organización"
+        author="Safe"
+      />
+      <Message
+        pfp={gacc}
+        message="Dale Iré haciendo lo principal primero"
+        author="gacc_15"
+      />
+      <Message
+        pfp={gacc}
+        message="Sep, entonces yo q podría hacer xD"
+        author="gacc_15"
+      />
+      <Message pfp={safe} message="elige register o login" author="Safe" />
+      <Message
+        pfp={gacc}
+        message="Iré por lo difícil Register"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="vale pues haz esa parte y yo hago el login"
+        author="Safe"
+      />
+      <Message
+        pfp={keven}
+        message="Se viene lo chido"
+        author="! IrregularDev"
+      />
+      <Message
+        pfp={gacc}
+        message="Una pregunta, yo haría la db?"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="Pues depende, yo tambien quiero aprender a hacerla, entonces supongo que podriamos dividirnos eso si, habria que tener muy buena organización"
+        author="Safe"
+      />
+      <Message
+        pfp={gacc}
+        message="Dale Iré haciendo lo principal primero"
+        author="
+              gacc_15"
+      />
+      <Message
+        pfp={gacc}
+        message="Sep, entonces yo q podría hacer xD"
+        author="gacc_15"
+      />
+      <Message pfp={safe} message="elige register o login" author="Safe" />
+      <Message
+        pfp={gacc}
+        message="Iré por lo difícil Register"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="vale pues haz esa parte y yo hago el login"
+        author="Safe"
+      />
+      <Message
+        pfp={keven}
+        message="Se viene lo chido"
+        author="! IrregularDev"
+      />
+      <Message
+        pfp={gacc}
+        message="Una pregunta, yo haría la db?"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="Pues depende, yo tambien quiero aprender a hacerla, entonces supongo que podriamos dividirnos eso si, habria que tener muy buena organización"
+        author="Safe"
+      />
+      <Message
+        pfp={gacc}
+        message="Dale Iré haciendo lo principal primero"
+        author="gacc_15"
+      />
+      <Message
+        pfp={gacc}
+        message="Sep, entonces yo q podría hacer xD"
+        author="gacc_15"
+      />
+      <Message pfp={safe} message="elige register o login" author="Safe" />
+      <Message
+        pfp={gacc}
+        message="Iré por lo difícil Register"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="vale pues haz esa parte y yo hago el login"
+        author="Safe"
+      />
+      <Message
+        pfp={keven}
+        message="Se viene lo chido"
+        author="! IrregularDev"
+      />
+      <Message
+        pfp={gacc}
+        message="Una pregunta, yo haría la db?"
+        author="gacc_15"
+      />
+      <Message
+        pfp={safe}
+        message="Pues depende, yo tambien quiero aprender a hacerla, entonces supongo que podriamos dividirnos eso si, habria que tener muy buena organización"
+        author="Safe"
+      />
+      <Message
+        pfp={gacc}
+        message="Dale Iré haciendo lo principal primero"
+        author="gacc_15"
+      />
+    </>
+  );
+}
+
+export default MessageTest;
