0d7d3181d254fc9968ac0829bdda3846bae726f2
Safestt <zakbass280@gmail.com>
2025-03-24 21:51:06 -0500

    Se configura el main del api donde se conectarán las futuras rutas

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes (ajústalo según sea necesario)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP
+    allow_headers=["*"],  # Permite todos los encabezados
+    expose_headers=["*"],  # Asegurar que el frontend pueda leer las respuestas del servidor
+)
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "api activa"}
