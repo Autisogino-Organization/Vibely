28cc68b1035875d22ba09386c3f174adf3740265
Safestt <zakbass280@gmail.com>
2025-03-25 22:05:05 -0500

    database file creation

--- routers/auth_routes.py
+++ routers/auth_routes.py
@@ -3,6 +3,6 @@ from utils.security import verify_password
 
 router = APIRouter(prefix="/auth",tags=["auth"])
 
-@router.post("/auth/login")
+@router.post("/auth/login",description="Login user with credentials")
 async def login(email:str,password:str):
-    return {"message":"funciona"}
+    
