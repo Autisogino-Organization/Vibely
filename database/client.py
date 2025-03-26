cee0f41eb5f78b0d3418cb5bbcaf36dbb84e12d9
Safestt <zakbass280@gmail.com>
2025-03-25 22:18:21 -0500

    dotenv added

--- database/client.py
+++ database/client.py
@@ -1,9 +1,8 @@
-
+import config
 from supabase import create_client
 
 SUPABASE_URL = "https://iqnyabathkjaqzqommeh.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxbnlhYmF0aGtqYXF6cW9tbWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NzM0NjMsImV4cCI6MjA1ODQ0OTQ2M30.khz65rw2AwOb1paOP4JsZWCqyNqMQPKkY22hB2SDBIk"
-
+SUPABASE_KEY = config.SECRET_DATABASE_KEY
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
