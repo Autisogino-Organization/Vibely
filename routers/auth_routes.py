ec926d10a2796cb3ab06685c6e33224c6a526184
Safestt <zakbass280@gmail.com>
2025-03-25 21:52:09 -0500

    Create login scheme

--- routers/auth_routes.py
+++ routers/auth_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
-
+from utils.security import verify_password
 
 router = APIRouter(prefix="/auth",tags=["auth"])
 
 @router.post("/auth/login")
-async def login():
+async def login(email:str,password:str):
     return {"message":"funciona"}
