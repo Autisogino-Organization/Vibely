3cddf6c349eace03aa536d00445d19d3b12e226a
Safestt <zakbass280@gmail.com>
2025-03-25 21:47:19 -0500

    Added verify password hashed

--- utils/security.py
+++ utils/security.py
@@ -1,1 +1,15 @@
-from fastapi import 
+import bcrypt
+
+
+
+def verify_password(password:str, hashed_password:str):
+    """
+    Transforma ambas contraseñas en formato de bytes
+    """
+    password_bytes = password.encode()
+    hashed_bytes = hashed_password.encode() 
+    #Se valida si los bytes coinciden
+    is_valid = bcrypt.checkpw(password_bytes,hashed_bytes)
+    return is_valid
+
+
